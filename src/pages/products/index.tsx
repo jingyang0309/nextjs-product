16756058206434cafd589f0cf3ef1a453211316f
Dio chang <jingyang0309@gmail.com>
2023-05-26 16:35:53 +0800

    css: products css module ok

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,8 +1,8 @@
 import useSWR from "swr";
 import ProductCard from "../../components/ProductCard";
 import { Product } from "../../../fake-data";
-import { PageTitle, ProductGallery } from "./products.style";
 import Head from "next/head";
+import productsCss from "./products.module.css";
 
 const fetcher = (url: string) =>
   fetch(`/api/products`).then((res) => {
@@ -17,17 +17,17 @@ const Home = () => {
 
   return (
     <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <title>商品頁CSR</title>
-    </Head>
-      <PageTitle>商品列表</PageTitle>
-      <ProductGallery>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>商品頁CSR</title>
+      </Head>
+      <h1 className={productsCss.pageTitle}>商品列表</h1>
+      <div className={productsCss.productGallery}>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
-      </ProductGallery>
+      </div>
     </>
   );
 };
